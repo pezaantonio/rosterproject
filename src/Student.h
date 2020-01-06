e7dcebe2446899ebf40638e6ea8558e576fb634d
Antonio Peza <anthony@aptechauto.com>
2020-01-05 21:31:00 -0800

    Added Degree.h and degree type to student.h

--- src/Student.h
+++ src/Student.h
@@ -9,6 +9,7 @@
 */
 #include <iostream>
 #include <string>
+#include "Degree.h"
 #pragma once // This is telling the the source file to only run this once
 
 using namespace std;
@@ -16,7 +17,7 @@ using namespace std;
 class Student
 {
 public:
-    Student(string firstName, string lastName, string studEmail, string studID, int studAge, int* daysLeft); //This is a parameterized constructor for the student class
+    Student(string firstName, string lastName, string studEmail, string studID, int studAge, int* daysLeft, Degree degreeType); //This is a parameterized constructor for the student class
 
     string getFirstName(); //This will get the first name of the student
     string getLastName(); //This will get the last name of the student
@@ -28,14 +29,15 @@ public:
 private:
     // These are the variables that are going to make up the student info
     string ifirstName, ilastName, istudEmail, istudID;
+    Degree idegreeType;
     int istudAge;
     int daysLeft[3];
 };
 
 
 // This is my parameterized constructor
-Student::Student(string firstName, string lastName, string studEmail, string studID, int studAge, int* daysLeft)
-    :ifirstName(firstName), ilastName(lastName), istudEmail(studEmail), istudID(studID), istudAge(studAge), idaysLeft(daysLeft)
+Student::Student(string firstName, string lastName, string studEmail, string studID, int studAge, int* daysLeft, Degree degreeType)
+    :ifirstName(firstName), ilastName(lastName), istudEmail(studEmail), istudID(studID), istudAge(studAge), idaysLeft(daysLeft), idegreeType(degreeType)
 {}
 
 /*
